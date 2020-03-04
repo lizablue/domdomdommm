3f998e2e8ff30998e75881f250472f270820c431
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-04 06:25:00 -0600

    id text appears on div

--- scripts.js
+++ scripts.js
@@ -1,18 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // create button
     let button = document.createElement('button');
     let btnText = document.createTextNode('Add Square');
-    
     button.appendChild(btnText);
     document.body.appendChild(button);
 
+    // add squares on button click
     button.addEventListener("click", function () {
         let square = document.createElement('div');
         square.className = "shape";
         square.style.backgroundColor = "black";
-        square.style.display = "inline-flex";
-        square.style.height = 100;
-        square.style.width = 100;
+        square.style.display = "inline-block";
         document.body.appendChild(square);
+        
+        // assign id to div with increasing number 
+        let id = document.getElementsByClassName("shape");
+        for(let i = 0; i < id.length; i++) {
+            id[i].setAttribute("id", i);
+        }
+        
+        // show div id on mouseover
+        square.addEventListener("mouseover", function () {
+            let idValue = square.id
+            let divId = document.createTextNode("ID=" + idValue)
+            square.appendChild(divId)
+            
+            // event.target.textContent = "";
+            // let divText = document.createTextNode('div #id');
+            // square.appendChild(divText);
+        })
     })
 
+   
 })
