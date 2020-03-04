c576f5eaf8ddfd45e8aa3090011dff5c7095820b
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-03 20:24:12 -0600

    squares on button click

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     let button = document.createElement('button');
     let btnText = document.createTextNode('Add Square');
-
+    
     button.appendChild(btnText);
     document.body.appendChild(button);
+
+    button.addEventListener("click", function () {
+        let square = document.createElement('div');
+        square.className = "shape";
+        square.style.backgroundColor = "black";
+        square.style.display = "inline-flex";
+        square.style.height = 100;
+        square.style.width = 100;
+        document.body.appendChild(square);
+    })
+
 })
