ac6a7c139fd2b18ae7bf9bc8913cf30d884853f5
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-04-18 09:09:52 -0500

    add styling to squares

--- scripts.js
+++ scripts.js
@@ -4,72 +4,71 @@ const button = document.createElement('button');
 let btnText = document.createTextNode('Add Square');
 button.appendChild(btnText);
 document.body.appendChild(button);
-
 button.style.display = "block";
+
+const container = document.createElement('main');
+document.body.appendChild(container);
 
 let counter = 1;
 
 class Box {
     constructor() {
-         this.div = document.createElement('div');
-         this.value = document.createTextNode(counter);
-         console.log(this);
+        // element creation
+        this.div = document.createElement('div');
+        this.value = document.createTextNode(counter);
+        //attributes
+        this.div.className = "shape";
+        container.appendChild(this.div);
+        // increase box id
+        this.div.id = counter;
+        // let divId = document.createTextNode("id=" + idValue)
+        // show div id on mouseover
+        this.div.addEventListener("mouseover", () => {
+            this.div.appendChild(this.value)
+        })
     }
 }
 
+
+
 // add squares on button click
 button.addEventListener("click", function () {
-    square.className = "shape";
-    square.style.backgroundColor = "black";
-    square.style.display = "inline-block";
-    document.body.appendChild(square);
+    new Box();
+    counter++;
 
-    // assign id to div with increasing number 
-    let id = document.getElementsByClassName("shape");
-    for (let i = 0; i < id.length; i++) {
-        id[i].setAttribute("id", i);
-    }
 
-    let idValue = square.id
-    let divId = document.createTextNode("id=" + idValue)
+        // // stop displaying id text on div
+        // square.addEventListener("mouseout", function () {
+        //     let showId = square.appendChild(divId)
+        //     square.removeChild(showId);
+        // })
 
-    // show div id on mouseover
-    square.addEventListener("mouseover", function () {
-        square.appendChild(divId)
-    })
+        // // change background color on click
+        // let colorArray = ["navy", "gold", "seagreen", "lightblue"];
+        // let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
 
-    // stop displaying id text on div
-    square.addEventListener("mouseout", function () {
-        let showId = square.appendChild(divId)
-        square.removeChild(showId);
-    })
+        // square.addEventListener("click", function () {
+        //     square.style.backgroundColor = randomColor;
+        // })
 
-    // change background color on click
-    let colorArray = ["navy", "gold", "seagreen", "lightblue"];
-    let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+        // // remove siblings on double click
+        // square.addEventListener("dblclick", function () {
+        //     let olderSibling = square.nextElementSibling;
+        //     let youngerSibling = square.previousElementSibling;
 
-    square.addEventListener("click", function () {
-        square.style.backgroundColor = randomColor;
-    })
-
-    // remove siblings on double click
-    square.addEventListener("dblclick", function () {
-        let olderSibling = square.nextElementSibling;
-        let youngerSibling = square.previousElementSibling;
-
-        if (idValue % 2 === 0) {
-            //even numbers
-            if (!olderSibling) {
-                alert('%cThe shape you are trying to remove does not exist', 'background: red; color: white');
-                return;
-            }
-            document.body.removeChild(olderSibling);
-        } else { //odd numbers
-            if (!youngerSibling || youngerSibling === document.querySelector('button')) {
-                alert('%cThe shape you are trying to remove does not exist', 'background: red; color: white');
-                return;
-            }
-            document.body.removeChild(youngerSibling);
-        }
-    })
+        //     if (idValue % 2 === 0) {
+        //         //even numbers
+        //         if (!olderSibling) {
+        //             alert('%cThe shape you are trying to remove does not exist', 'background: red; color: white');
+        //             return;
+        //         }
+        //         document.body.removeChild(olderSibling);
+        //     } else { //odd numbers
+        //         if (!youngerSibling || youngerSibling === document.querySelector('button')) {
+        //             alert('%cThe shape you are trying to remove does not exist', 'background: red; color: white');
+        //             return;
+        //         }
+        //         document.body.removeChild(youngerSibling);
+        //     }
+        // })
 })
