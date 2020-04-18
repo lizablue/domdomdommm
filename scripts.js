2ee8238a6621a555728b16a5fe380da67c12ed63
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-04-18 09:44:45 -0500

    convert lab to a class based project

--- scripts.js
+++ scripts.js
@@ -13,62 +13,43 @@ let counter = 1;
 
 class Box {
     constructor() {
-        // element creation
         this.div = document.createElement('div');
         this.value = document.createTextNode(counter);
-        //attributes
         this.div.className = "shape";
         container.appendChild(this.div);
-        // increase box id
         this.div.id = counter;
-        // let divId = document.createTextNode("id=" + idValue)
-        // show div id on mouseover
-        this.div.addEventListener("mouseover", () => {
-            this.div.appendChild(this.value)
+        this.div.addEventListener("mouseover", () => this.div.appendChild(this.value));
+        this.div.addEventListener("mouseout", () => this.div.removeChild(this.value));
+        this.div.addEventListener("click", () => {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            this.div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        })
+        // remove siblings on double click
+        this.div.addEventListener("dblclick", () => {
+            let olderSibling = this.div.nextElementSibling;
+            let youngerSibling = this.div.previousElementSibling;
+            if (this.value % 2 === 0) {
+                //even numbers
+                if (!olderSibling) {
+                    console.log('%cThe shape you are trying to remove does not exist', 'background: red; color: white');
+                    return;
+                }
+                container.removeChild(olderSibling);
+            } else { //odd numbers
+                if (!youngerSibling || youngerSibling === document.querySelector('button')) {
+                    console.log('%cThe shape you are trying to remove does not exist', 'background: red; color: white');
+                    return;
+                }
+                container.removeChild(youngerSibling);
+            }
         })
     }
 }
-
-
 
 // add squares on button click
 button.addEventListener("click", function () {
     new Box();
     counter++;
-
-
-        // // stop displaying id text on div
-        // square.addEventListener("mouseout", function () {
-        //     let showId = square.appendChild(divId)
-        //     square.removeChild(showId);
-        // })
-
-        // // change background color on click
-        // let colorArray = ["navy", "gold", "seagreen", "lightblue"];
-        // let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
-
-        // square.addEventListener("click", function () {
-        //     square.style.backgroundColor = randomColor;
-        // })
-
-        // // remove siblings on double click
-        // square.addEventListener("dblclick", function () {
-        //     let olderSibling = square.nextElementSibling;
-        //     let youngerSibling = square.previousElementSibling;
-
-        //     if (idValue % 2 === 0) {
-        //         //even numbers
-        //         if (!olderSibling) {
-        //             alert('%cThe shape you are trying to remove does not exist', 'background: red; color: white');
-        //             return;
-        //         }
-        //         document.body.removeChild(olderSibling);
-        //     } else { //odd numbers
-        //         if (!youngerSibling || youngerSibling === document.querySelector('button')) {
-        //             alert('%cThe shape you are trying to remove does not exist', 'background: red; color: white');
-        //             return;
-        //         }
-        //         document.body.removeChild(youngerSibling);
-        //     }
-        // })
 })
